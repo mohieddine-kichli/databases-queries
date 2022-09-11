25b29dbc3c85ca09f496e1e067f172cabe50f8f5
mohieddine-kichli <mohieddine.kichli@gmail.com>
2022-09-11 22:37:20 +0300

    add query 2 ex2

--- courses.sql
+++ courses.sql
@@ -1,5 +1,15 @@
 -- Query 1
 SELECT name
 FROM rooms
-WHERE capacity >= 100
+WHERE capacity >= 100;
+
+-- Query 2
+SELECT name
+FROM courses
+WHERE start_time = (SELECT MIN(start_time)
+                    FROM courses);
+
+
+
+
 
