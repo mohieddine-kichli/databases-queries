f1fd7bc2c7d173723b968a5d26a0513c39da1ed4
mohieddine-kichli <mohieddine.kichli@gmail.com>
2022-09-11 23:40:29 +0300

    add query 6 ex2

--- courses.sql
+++ courses.sql
@@ -28,6 +28,26 @@ FROM students
 WHERE id not in (SELECT students_id
                  FROM enrolls);
 
+-- Query 6
+
+SELECT COUNT(*)
+FROM students
+WHERE id IN (SELECT students_id
+              FROM enrolls
+              WHERE courses_crn IN (SELECT crn
+                                    FROM courses
+                                    WHERE name = "CSC275") AND students_id IN (SELECT students_id
+                                                                                FROM majorsin
+                                                                                WHERE departments_id IN (SELECT id
+                                                                                                          FROM departments
+                                                                                                          WHERE name = "CS")))
+
+
+-- Query 7
+
+
+                                                                              					
+                    
 
 
 
